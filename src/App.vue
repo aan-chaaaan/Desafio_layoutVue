a304264a8df5aa6387f7284eac2afe68d42abdc6
angela <angela.valdebenito.v@gmail.com>
2021-10-05 17:32:43 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,47 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <v-toolbar extended class="mb-3" color="pink">
+      <v-toolbar-side-icon></v-toolbar-side-icon>
+      <v-toolbar-title>Desafio</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn text>Inicio</v-btn>
+        <v-btn text>Buscar</v-btn>
+        <v-btn text>Acerca de</v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+    <v-card
+      mt4 ma3
+      max-width="374"
+      v-for="(personaje, index) in $store.state.personajes"
+      :key="index"
+    >
+      <v-img height="250" :src="personaje.imagen"></v-img>
+      <v-card-title>{{ personaje.nombre }}</v-card-title>
+    </v-card>
+
+    <v-flex d-flex>
+      <v-layout wrap>
+        <v-flex md4 v-for="(personaje, index) in $store.state.personajes"
+            :key="index">
+          <v-card
+            class="card-container"
+          >
+            <v-img height="250" :src="personaje.imagen"></v-img>
+            <v-card-title>{{ personaje.nombre }}</v-card-title>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-flex>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
   name: "App",
-  components: {
-    HelloWorld,
-  },
+
+  data: () => ({
+    //
+  }),
 };
 </script>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
